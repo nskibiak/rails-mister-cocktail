06f111413c820ba8a70fbc07050f5fd28975d6d5
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-15 08:45:33 +0900

    added delete dose functionality to cocktail show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,7 +16,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
-
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
