9e6b6771112c2c53104042399fd33d69c08cdc4a
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-14 21:17:22 +0900

    created doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,15 @@
 class CocktailsController < ApplicationController
   def home
+    @cocktails = Cocktail.all
+    @ingredients = Ingredient.all
+  end
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def new
+
+  end
+  def create
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
   get 'cocktails/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails, only: [ :home, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create ]
+  end
 
   root to: 'cocktails#home'
 end
