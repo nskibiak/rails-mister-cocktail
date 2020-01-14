91ecac1faf886203f9ab77147e055189371fd498
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-14 21:26:05 +0900

    creating doses create route

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,4 +4,8 @@ class DosesController < ApplicationController
 
   def create
   end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'cocktails/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [ :home, :show, :new, :create ] do
-    resources :doses, only: [ :new, :create ]
+    resources :doses, only: [ :new, :create , :destroy]
   end
 
   root to: 'cocktails#home'
