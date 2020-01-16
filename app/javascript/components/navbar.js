08380dc32a6a2d8c0418df0dbdf0e7225e158c98
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-16 20:00:46 +0900

    added js function to make navbar appear on scroll down

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,14 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.remove('invisible');
+      } else {
+        navbar.classList.add('invisible');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 import 'bootstrap';
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
 $(document).ready(function(){
 
   $('#arrow').click(function(){
@@ -18,6 +20,8 @@ $(document).ready(function(){
    });
 
 });
+
+initUpdateNavbarOnScroll();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
