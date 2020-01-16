c41161051df7b785dc14afa1444f69d56e5f1c0b
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-16 17:29:07 +0900

    added banner image and redid back to cocktail links with specific anchor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,15 @@ require("channels")
 
 import 'bootstrap';
 
+$(document).ready(function(){
+
+  $('#arrow').click(function(){
+    var h = $('.banner').outerHeight();
+    $('body,html').animate({scrollTop: h},1000);
+   });
+
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
