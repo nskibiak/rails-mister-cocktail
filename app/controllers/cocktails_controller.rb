ddd66384e52b395c1736629d0d558809ab0cad4c
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-18 14:26:03 +0900

    changed config to support cloudinary

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :image, :directions)
+    params.require(:cocktail).permit(:name, :image, :directions, :photo)
   end
 end
